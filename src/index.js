2e00abced2fd6306ee058c3f3846145f1256e4fe
SerhiiChorpita <sergiochorpita@gmail.com>
2022-09-03 22:26:49 +0200

    Analysis of local server operation

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+const express = require('express');
+const port = 3002;
+const bodyParser = require('body-parser');
+const routes = require('../src/app/routes/routes.js')
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+
+    console.log(`Server listening on port: ${server.address().port}`);
+});
